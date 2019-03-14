5cceca9338b4d393e8be189d58069c6d3455f67a
fimami <florian-imami@hotmail.com>
2019-03-14 18:11:09 +0100

    UserService finished version 2

--- src/main/java/ch/uzh/ifi/seal/soprafs19/service/UserService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/service/UserService.java
@@ -8,6 +8,7 @@ import ch.uzh.ifi.seal.soprafs19.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,7 +24,7 @@ public class UserService {
 
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(@Qualifier("userRepository") UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
